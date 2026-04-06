8eb71bef12c2ab878e51e23a57ddbf0c990ca024
Diego Rocha <diegoarocha1369@gmail.com>
2026-04-06 10:47:14 -0300

    feat(script.js): alert e console das variáveis adicionado

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,14 @@ let quantidadeVinho = parseInt(prompt("Digite a quantidade de garrafas no estoqu
 let precoVinho = parseFloat(prompt("Digite o preço do vinho:"));
 
 alert("O Cadastro foi realizado com sucesso! Veja mais detalhes no console.");
+
+alert(`Nome do Vinho já está disponível no console!`);
+console.log("Nome do Vinho: " + nomeVinho);
+alert(`Tipo de Vinho já está disponível no console!`);
+console.log("Tipo do Vinho: " + tipoVinho);
+alert(`Safra do Vinho já está disponível no console!`);
+console.log("Safra do Vinho: " + safraVinho);
+alert(`Preço do Vinho já está disponível no console!`);
+console.log("Preço: R$ " + precoVinho.toFixed(2));
+alert(`Quantidade no Estoque já está disponível no console!`);
+console.log("Quantidade no Estoque: " + quantidadeVinho);
